be18a30c09a86c2d07463898e6e3003a5dc0f3ec
Vn-Boy <0939899928za.com@gmail.com>
2024-02-23 15:52:28 +0700

    Add project files.

---
+++ QuanLyDKMonHocVaThuPhiSV/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuanLyDKMonHocVaThuPhiSV
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ QuanLyDKMonHocVaThuPhiSV/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuanLyDKMonHocVaThuPhiSV
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
